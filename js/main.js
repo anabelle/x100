939e170d0db4e0954c353d3bbf334d275a3f692e
Anabelle Handdoek <github@huellaspyp.com>
2015-09-28 12:54:37 -0500

    primera version funcional

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$( document ).ready(function() {
+	var lista_nombres = shuffle(nombres);
+	var lista_colores = shuffle(colores);
+
+	function shuffle(o){
+		for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+		return o;
+	}
+
+	function randomNoRepeats(array) {
+	  var copy = array.slice(0);
+	  return function() {
+	    if (copy.length < 1) { copy = array.slice(0); }
+	    var index = Math.floor(Math.random() * copy.length);
+	    var item = copy[index];
+	    copy.splice(index, 1);
+	    return item;
+	  };
+	}
+
+	var colors = randomNoRepeats( lista_colores );
+
+	var i;
+	for (i = 0; i < lista_nombres.length; ++i) {
+		console.log( lista_nombres[i] );
+		$('#nombres .row').append(
+			'<div style="color:'+colors()+'" class="flex-item">'+lista_nombres[i]+'</div>'
+		);
+	}
+});
